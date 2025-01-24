dfa6e99631c62a623f293c72986f5ec7db14fb23
op06072 <op06072@gmail.com>
2025-01-25 02:24:56 +0900

    Refactor: refactor the package dependency

--- Package.swift
+++ Package.swift
@@ -18,13 +18,9 @@ let package = Package(
             , targets: ["SDWebImageAVIF"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/awxkee/libaom.swift.git", "1.1.0"..<"1.2.0"),
-        .package(url: "https://github.com/awxkee/libdav1d.swift.git", "1.2.0"..<"1.3.0"),
-        .package(url: "https://github.com/awxkee/libyuv.swift.git", "1.1.0"..<"1.2.0"),
-        .package(url: "https://github.com/op06072/libwebp-ios.git", "1.2.0"..<"1.3.0"),
         .package(url: "https://github.com/kean/Nuke.git", "12.0.0"..<"13.0.0"),
-        .package(url: "https://github.com/awxkee/libsvtav1enc.swift", "1.1.0"..<"1.2.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", branch: "master"),
+        .package(url: "https://github.com/SDWebImage/libavif-Xcode.git", from: "0.11.1")
     ],
     targets: [
         .target(
@@ -38,7 +34,7 @@ let package = Package(
             dependencies: ["avifc"]),
         .target(name: "avifc",
                 dependencies: [
-                    .target(name: "libavif"),
+                    .product(name: "libavif", package: "libavif-Xcode"),
                     .product(name: "SDWebImage", package: "SDWebImage"),
                 ],
                 publicHeadersPath: "include",
@@ -49,34 +45,6 @@ let package = Package(
                 ],
                 linkerSettings: [
                     .linkedFramework("Accelerate")
-                ]),
-        .target(name: "libavif",
-                dependencies: [
-                    .product(name: "libaom", package: "libaom.swift"),
-                    .product(name: "libdav1d", package: "libdav1d.swift"),
-                    .product(name: "libyuv", package: "libyuv.swift"),
-                    .product(name: "libwebp-ios", package: "libwebp-ios"),
-                    .product(name: "libSvtAv1Enc", package: "libsvtav1enc.swift")],
-                publicHeadersPath: "include",
-                cSettings: [
-                    .define("DEBUG", to: "1"),
-                    .define("AVIF_CODEC_AOM_ENCODE", to: "1"),
-                    .define("AVIF_CODEC_AOM", to: "1"),
-                    .define("AVIF_CODEC_DAV1D", to: "1"),
-                    .define("AVIF_LIBYUV_ENABLED", to: "1"),
-                    .define("AVIF_LIBSHARPYUV_ENABLED", to: "1"),
-                    .define("AVIF_ENABLE_EXPERIMENTAL_YCGCO_R", to: "1"),
-                    .define("AVIF_ENABLE_EXPERIMENTAL_GAIN_MAP", to: "1"),
-                    .define("AVIF_CODEC_SVT", to: "1")
-                ],
-                cxxSettings: [
-                    .define("DEBUG", to: "1"),
-                    .define("AVIF_CODEC_AOM_ENCODE", to: "1"),
-                    .define("AVIF_CODEC_AOM", to: "1"),
-                    .define("AVIF_CODEC_DAV1D", to: "1"),
-                    .define("AVIF_LIBYUV_ENABLED", to: "1"),
-                    .define("AVIF_LIBSHARPYUV_ENABLED", to: "1"),
-                    .define("AVIF_CODEC_SVT", to: "1")
                 ]),
         .target(name: "SDWebImageAVIF",
                 dependencies: [
